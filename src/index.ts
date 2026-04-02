d269ac06c55519325d071f6173d0541e70c213a2
duyphong204 <duyphong242004@gmail.com>
2026-04-02 11:40:41 +0700

    update index file

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,13 @@ import "./config/passport.config";
 import http from "http";
 import { initializeSocket } from "./lib/socket";
 // import viewRoutes from './routes/view.route'
-import dotenv from "dotenv";
-import express from "express";
 import connecDB from "./config/database.config";
 import rootRouter from "./routes/index";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger.config";
-
-dotenv.config();
+import cors from 'cors'
+import express, { Request, Response } from 'express'
+import 'dotenv/config'
 
 const app = express();
 const server = http.createServer(app);
@@ -32,11 +31,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({
+  origin: Env.FRONTEND_ORIGIN,
+  credentials: true
+}))
 app.use(passport.initialize());
 
-// Routes views
-// app.use("/", viewRoutes);
+app.use(passport.initialize())
 
 // Routes API
 app.use("/api", rootRouter);
@@ -44,17 +45,7 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(errorHandler);
 
-const startServer = async () => {
-  try {
-    await connecDB();
-
-    app.listen(Env.PORT, () => {
-      console.log(`Server is running at http://localhost:${Env.PORT}`);
-    });
-  } catch (error) {
-    console.error("Lỗi khởi động hệ thống:", error);
-    process.exit(1);
-  }
-};
-
-startServer();
+server.listen(Env.PORT, async () => {
+  await connecDB()
+  console.log(`server running ${Env.PORT}`)
+})
